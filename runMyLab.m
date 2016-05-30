2c4a6e13a3213b17ef77226b165cab3154a73afd
Theodore Nguyen <drautuna@laguna.pic.ucla.edu>
2016-05-29 18:11:44 -0700

    sdasfix script for runMyLab.m

--- runMyLab.m
+++ runMyLab.m
@@ -2,14 +2,14 @@ clear all
 data_conversion
 train_num = 20000;
 N = 32560;
-[w, b] = softsvm(data(1:train_num,:), over50k(1:train_num), 0.005)	%used gamma = 0.005 in the lab, don't know why
-prediction = data(N-train_num:N,:) * w' + b
-results = []
+[w, b] = softsvm(data(1:train_num,:), over50k(1:train_num), 0.005);	%used gamma = 0.005 in the lab, don't know why
+prediction = data((train_num+1):N,:) * w' + b;
+results = zeros(1, N - train_num);
 for i = 1:N-train_num
-	if sign(prediction(i)) == sign(over50k(i))
-		results(end + 1) = 1;
+	if sign(prediction(i)) == sign(over50k(train_num + i))
+		results(i) = 1;
 	else
-		results(end + 1) = 0;
+		results(i) = 0;
 	end
 end
 sum(results) / (N - train_num)
