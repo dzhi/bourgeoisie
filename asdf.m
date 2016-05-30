e12491ae5fb8f753e6b90d1751e475b950c13bdb
Dillon Zhi <dzhi@laguna.pic.ucla.edu>
2016-05-29 19:09:45 -0700

    Fixed uncaught merge problems from previous merge

--- asdf.m
+++ asdf.m
@@ -1,12 +1,12 @@
 % Modify this line to choose the specific subset of the data attributes you
 % want to use for prediction
-%limited_data = data;
-limited_data = [age education];
+limited_data = data;
+%limited_data = [age education];
 perm = randperm(N);
 data_reordered = limited_data(perm, :);
 over50k_reordered = over50k(perm, :);
 
-m = 4000;
+m = 1000;
 X_train = data_reordered(1:m, :);
 y = over50k_reordered(1:m);
 svm_model = fitcsvm(X_train, y, ...
@@ -21,12 +21,8 @@ X_test = data_reordered(m+1:N, :);
 correct = label==over50k_reordered(m+1:N);
 correct_fraction = mean(correct);
 
-correct_for_over50k = correct(logical(over50k_reordered(m+1:N)));
+correct_for_over50k = correct(over50k_reordered(m+1:N)==1);
 correct_for_over50k_fraction = mean(correct_for_over50k);
 
-correct_for_under50k = correct(~logical(over50k_reordered(m+1:N)));
+correct_for_under50k = correct(over50k_reordered(m+1:N)==-1);
 correct_for_under50k_fraction = mean(correct_for_under50k);
-
-predicted_over50k_fraction = mean(label);
-
-over50k_fraction = mean(over50k_reordered);
