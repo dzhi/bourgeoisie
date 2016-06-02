c8e205982d428f425383e3b7439346cddd78d683
Dillon Zhi <dillon.zhi@gmail.com>
2016-06-02 11:22:46 -0700

    Changes to asdf.m

--- asdf.m
+++ asdf.m
@@ -1,17 +1,23 @@
 % Modify this line to choose the specific subset of the data attributes you
 % want to use for prediction
 limited_data = data;
+% Example:
 %limited_data = [age education];
-perm = randperm(N);
-data_reordered = limited_data(perm, :);
-over50k_reordered = over50k(perm, :);
+
+data_reordered = data;
+over50k_reordered = over50k;
+% Uncomment to use a randomly selected training set instead
+%perm = randperm(N);
+%data_reordered = limited_data(perm, :);
+%over50k_reordered = over50k(perm, :);
 
 m = 1000;
 X_train = data_reordered(1:m, :);
 y = over50k_reordered(1:m);
+
 svm_model = fitcsvm(X_train, y, ...
         'Standardize', true, ...
-        'KernelFunction', 'linear', ...
+        'KernelFunction', 'linear', ... % can replace 'linear' with 'gaussian'
         'KernelScale', 'auto', ...
         'BoxConstraint', 1);
 
